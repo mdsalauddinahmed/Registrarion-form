7620dd3efe1ea30c680335c007e1783022be5442
salauddinAmir <mdsalauddinahmed.amir@gmail.com>
2022-11-07 21:29:20 +0600

    added

---
+++ src/form/Form.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Form = () => {
+    return (
+        <>
+        <form  className="container">
+        <div className="header">
+            <h1>Registration Form</h1>
+        </div>
+        <div>
+             <input type="text" placeholder='Enter your Name' name='name' ></input>
+        </div>
+        <div>
+             <input type="email" placeholder='Enter your Email' name='email' ></input>
+        </div>
+        <div>
+             <input type="password" placeholder='Enter your Password' name='password' ></input>
+        </div>
+        <div>
+            <button type='submit'>Submit</button>
+        </div>
+    </form>
+    </>
+    );
+};
+
+export default Form;
