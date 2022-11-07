26ff3848fed91592c2af15f36b86e54faf401a86
salauddinAmir <mdsalauddinahmed.amir@gmail.com>
2022-11-07 23:13:35 +0600

    added

--- src/form/Form.jsx
+++ src/form/Form.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
+import { useState } from 'react';
 
 const Form = () => {
+    const data ={name:"",email:"",password:""};
+    const [inputData,setInputData] =useState(data)
+    const handleData =(e)=>{
+        
+
+    }
     return (
         <>
         <form  className="container">
@@ -8,13 +15,13 @@ const Form = () => {
             <h1>Registration Form</h1>
         </div>
         <div>
-             <input type="text" placeholder='Enter your Name' name='name' ></input>
+             <input type="text" placeholder='Enter your Name' name='name' value={inputData.name} onChange={handleData} ></input>
         </div>
         <div>
-             <input type="email" placeholder='Enter your Email' name='email' ></input>
+             <input type="email" placeholder='Enter your Email' name='email' value={inputData.email} onChange={handleData} ></input>
         </div>
         <div>
-             <input type="password" placeholder='Enter your Password' name='password' ></input>
+             <input type="password" placeholder='Enter your Password' name='password'value={inputData.password} onChange={handleData}></input>
         </div>
         <div>
             <button type='submit'>Add form</button>
